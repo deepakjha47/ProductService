0b4ade7b79ee9d5b7e2ddb6fae04c13cccd2f892
djha93486 <djha93486@gmail.com>
2024-09-12 23:28:38 +0530

    initial setup

---
+++ src/main/java/com/dpanalytics/ProductServcie/ProductServcieApplication.java
@@ -0,0 +1,13 @@
+package com.dpanalytics.ProductServcie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServcieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServcieApplication.class, args);
+	}
+
+}
